facee76f8d1838eb24858084a79d30cc058c129c
Celyn Xie <luvmex@gmail.com>
2020-04-07 23:18:28 +0800

    upgrade gulp 4

--- dev/js/main.js
+++ dev/js/main.js
@@ -54,51 +54,6 @@ CELYN.disableHover = function(){
 
 ///////////////////////////////////
 
-CELYN.fullHeight = function(){
-	var winH = window.innerHeight ? window.innerHeight:$(window).height();
-	var winW = $(window).width();
-	$(".fullHeight").each(function(){
-		$(this).css({'height': winH +'px'});
-	});
-};
-
-CELYN.elementHeight = function(){
-	var winH = window.innerHeight ? window.innerHeight:$(window).height();
-	var winW = $(window).width();
-	var elementHeight = $('.elementHeight').width();
-	$('.elementHeight').css({'height': elementHeight});
-};
-
-
-CELYN.scrollDown = function(){
-
-    var scroll = $(window).scrollTop();
-    var headerOffset = $('header').offset();
-	var mainOffset = $('main').offset();
-    var headerHeight = $('header').height();
-    var values = (scroll - headerOffset.top)/headerHeight;
-	var scrollnext = $('#scrolldown');
-
-    //Scroll down
-    if( scroll > headerOffset.top ) {
-      	scrollnext.css({'opacity': 1 - (values*1.5), 'bottom': 30 - (values*60)});
-		if (scroll > mainOffset.top ) {
-			scrollnext.css({'opacity':'0','bottom':'0px'});
-		}
-    } else {
-		scrollnext.css({'opacity':'1','bottom':'30px'});
-	}
-
-};
-
-
-CELYN.scrollTop = function(){
-	$('#scrolltop').click(function(){
-		$('html, body').animate({scrollTop : 0}, 850, 'easeInOutExpo');
-		return false;
-	});
-};
-
 
 $(window).load(function() {
 });
@@ -106,56 +61,9 @@ $(window).load(function() {
 
 $(document).ready(function() {
 
-	CELYN.fullHeight();
-	CELYN.elementHeight();
-	CELYN.scrollTop();
-
-
-	// Nav scroll
-	var startScroll;
-	var lastScrollTop = 0;
-	var scrollOver = 5;
-	var navbarHeight = $('nav').outerHeight();
-	
 	$(window).scroll(function(event) {  
 
-
     	CELYN.disableHover();
-    	CELYN.scrollDown();
-
-
-    	//nav scroll
-		var startScroll = true;  
-
-		setInterval(function() {
-		    if (startScroll) {
-		        hasScrolled();
-		        startScroll = false;
-		    }
-		}, 250);
-
-		function hasScrolled() {
-		    var st = $(this).scrollTop();
-		    if(Math.abs(lastScrollTop - st) <= scrollOver)
-		        return;
-		    if (st > lastScrollTop && st > navbarHeight){
-		        $('nav').css({'top':'-100px'});
-		    } else {
-		        if(st + $(window).height() < $(document).height()) {
-		            $('nav').css({'top':'0px'});
-		        }
-		    }
-		    if(st >= 300){
-		    	$("nav").addClass("navScroll");
-		    	$('#scrolltop').css({'bottom':'50px', 'opacity':'1'});
-		    } else {
-		    	$("nav").removeClass("navScroll");
-		    	$('#scrolltop').css({'bottom':'-50px','opacity':'0'});
-		    }
-		    lastScrollTop = st;
-		};
-
-
 
 	});
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,108 @@
-var gulp = require('gulp'),
-  browserSync = require('browser-sync').create(),
-  notify = require('gulp-notify'),
-  rename = require('gulp-rename'),
-  concat = require('gulp-concat'),
-  del = require('del');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const notify = require('gulp-notify');
+const rename = require('gulp-rename');
+// const concat = require('gulp-concat');
+
+//script
+const uglify = require('gulp-uglify');
+const jshint = require('gulp-jshint');
+
+//image
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+//
+const stylus = require('gulp-stylus');
+const postcss = require('gulp-postcss');
+const rupture = require('rupture');
+const mqpacker = require('css-mqpacker');
+const sourcemaps = require('gulp-sourcemaps');
+//
+const pug = require('gulp-pug');
+//
+const del = require('del');
 
 
-// Static Server + watching scss/html files
-gulp.task('server', function() {
-    browserSync.init({
-        server: "./build/"
-    });
-});
-
-gulp.task('copy', function() {
-    gulp.src(['./dev/lib/script/**/*'])
-        .pipe(gulp.dest('./build/js/'))
-    gulp.src(['./dev/lib/css/**/*'])
-        .pipe(gulp.dest('./build/css/'))
-    gulp.src('./dev/lib/image/**/*')
-        .pipe(gulp.dest('./build/img/'))
-    gulp.src('./dev/lib/font/**/*')
-        .pipe(gulp.dest('./build/font/'))
-});
+const paths = {
+  src: 'dev/**',
+  dest: 'build'
+}
 
  //Before build file clean the old one
 gulp.task('clean', function(cb) {
-    del(['./build/'], cb)
+  del(['build'], cb)
 });
+
+
+//Compile the jshint scripts
+function scripts() {
+  return gulp.src('dev/js/*.js')
+    .pipe(jshint())
+    // .pipe(concat('js.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'))
+    .pipe(browserSync.stream())
+}
+exports.scripts = scripts;
 
 
 //Compile the layout to html
-gulp.task('layout', function() {
-  var pug = require('gulp-pug');
-  gulp.src('./dev/layout/**/!(_)*.pug')
+function layout() {
+  return gulp.src('dev/layout/**/!(_)*.pug')
       .pipe(pug())
-      .pipe(gulp.dest('./build/'))
+      .pipe(gulp.dest('build'))
       .pipe(browserSync.stream())
-});
+}
+exports.layout = layout;
 
 
 //Compile the stylus to css
-gulp.task('style', function(){
-  var stylus = require('gulp-stylus');
-    postcss = require('gulp-postcss'),
-      rupture = require('rupture'),
-      mqpacker = require('css-mqpacker'),
-    sourcemaps = require('gulp-sourcemaps'),
-    processors = [
+function style() {
+  const processors = [
     require('lost'),
     require('autoprefixer')({ browsers: ['last 5 version']}),
       require('css-mqpacker'),
       require('cssnano')({zindex: false, reduceIdents: false})
   ];
-  return gulp.src(['./dev/css/!(_)*.styl'])
+  return gulp.src(['dev/css/**/!(_)*.styl'])
     .pipe(sourcemaps.init())
     .pipe(stylus({ use: rupture(), compress: true }))
     .pipe(postcss(processors))
     // .pipe(sourcemaps.write('.'))
     // .pipe(concat('css.css'))
-    .pipe(gulp.dest('./build/css/'))
+    .pipe(gulp.dest('build/css'))
     .pipe(browserSync.stream())
-});
-
-
-//Compile the jshint scripts
-gulp.task('script', function() {
-  var uglify = require('gulp-uglify');
-  var jshint = require('gulp-jshint');
-  return gulp.src('./dev/js/*.js')
-    .pipe(jshint())
-    // .pipe(concat('js.js'))
-    // .pipe(uglify())
-    .pipe(gulp.dest('./build/js/'))
-    .pipe(browserSync.stream())
-});
-
+}
+exports.style = style;
 
 // Images
-gulp.task('image', function() {
-  var cache = require('gulp-cache');
-  var imagemin = require('gulp-imagemin');
-  return gulp.src('./dev/img/**/*')
+function image() {
+  return gulp.src('dev/img/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('./build/img/'))
+    .pipe(gulp.dest('build/img'))
     .pipe(browserSync.stream())
-});
-
+}
+gulp.task(image);
 
 // Watch the file if it change
-gulp.task('watch', function(){
-  gulp.watch('./dev/css/**/*.styl', ['style']);
-  gulp.watch('./dev/js/*.js', ['script']);
-  gulp.watch('./dev/layout/**/*.pug', ['layout']);
-  gulp.watch('./dev/img/**/*', ['image']);
-});
-
+function watch() {
+  browserSync.init({
+      server: "build"
+  });
+  gulp.watch('dev/css/**/*.styl', style);
+  gulp.watch('dev/js/*.js', scripts);
+  gulp.watch('dev/layout/**/*.pug', layout);
+}
+exports.watch = watch;
 
 gulp.task('copy', function () {
-    return gulp.src(['./dev/lib/**'])
-    .pipe(gulp.dest('./build/lib'));
+    return gulp.src(['dev/lib/**'])
+    .pipe(gulp.dest('build/lib'));
 });
 
 
-// Start compile stylus pug js
-gulp.task('compile', function(){
-    gulp.start('script', 'layout', 'style', 'image')
-});
+gulp.task('default', gulp.series(layout, style, scripts, image, watch));
 
 
-//GO
-gulp.task('default',['server'], function() {
-    gulp.start('watch', 'compile');
-});
 
 
